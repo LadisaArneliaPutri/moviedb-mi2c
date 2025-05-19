bd9724dc99709ba96b4ab18e9fb7f310b218e797
LadisaArneliaPutri <ldisa52@gmail.com>
2025-05-18 19:35:44 -1200

    seeding and factory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,8 +12,39 @@ class CategorySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+    public function run(): void 
+{
+    DB ::table('categories')->insert([
+        [
+            'category_name' => 'Action',
+            'description' => 'Film dengan adegan-adegan dengan penuh aksi dan ketegangan.',
+            'created_at' => now(),
+            'updated_at' => now(),    
+        ],
+        [
+            'category_name' => 'Comedy',
+            'description' => 'Film yang bertujuan untuk menghibur dan mengundang tawa.',
+            'created_at' => now(),
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Drama',
+            'description' => 'Film yang berfokus pada pengembangan karakter dan konflik emosional.',
+            'created_at' => now(),
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Sci-Fi',
+            'description' => 'Film dengan latar belakang ilmiah dan teknologi futuristik.',
+            'created_at' => now(),
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Romance',
+            'description' => 'Film yang berpusat pada kisah cinta dan hubungan romantis.',
+            'created_at' => now(),
+            'updated_at' => now(), 
+        ],
+    ]);
+}
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,21 +4,23 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
-     * @return void
+     * Run the database seeds.
      */
-    public function run()
+     
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+        // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(CategorySeeder::class);
+        Movie::factory(50)->create();
     }
 }
