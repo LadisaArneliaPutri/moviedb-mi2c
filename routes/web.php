f606f3fce5cc154845237f1091cc80799c78f2e0
LadisaArneliaPutri <ldisa52@gmail.com>
2025-05-25 19:16:38 -1200

    sending detail,web,moviecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
+
 <?php
 
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //Route::get('/', function () {
-  //  return view('welcome');
+   // return view('welcome');
 //});
-
-Route::get('/', [MovieController::class, 'homepage']);
+Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
+Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.detail');
