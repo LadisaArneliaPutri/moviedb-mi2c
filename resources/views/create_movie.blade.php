9ce05ff730c3e9de1c4c6fd65bbe84d2e6369863
LadisaArneliaPutri <ldisa52@gmail.com>
2025-05-25 21:43:48 -1200

    from detail

--- resources/views/create_movie.blade.php
+++ resources/views/create_movie.blade.php
@@ -1,46 +1,69 @@
 @extends('layouts.template')
 
 @section('content')
-    <h1 class="mb-4">Form Data Movie</h1>
-    
-    <form action="{{ route('movies.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        
-        <div class="form-group mb-3">
-            <label for="title">Title</label>
-            <input type="text" name="title" class="form-control" required>
-        </div>
+<h1>Input Movie</h1>
 
-        <div class="form-group mb-3">
-            <label for="synopsis">Synopsis</label>
-            <textarea name="synopsis" class="form-control" rows="4" required></textarea>
+<form action="{{ url('/create-movie') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    {{-- Title --}}
+    <div class="mb-3 row">
+        <label for="title" class="col-sm-2 col-form-label">Title</label>
+        <div class="col-sm-10">
+            <input type="text" name="title" class="form-control" id="title" required>
         </div>
+    </div>
 
-        <div class="form-group mb-3">
-            <label for="category">Category</label>
-            <select name="category_id" class="form-control" required>
-                <option value="">-- Pilih Kategori --</option>
+    {{-- Synopsis --}}
+    <div class="mb-3 row">
+        <label for="synopsis" class="col-sm-2 col-form-label">Synopsis</label>
+        <div class="col-sm-10">
+            <textarea name="synopsis" class="form-control" id="synopsis" rows="3" required></textarea>
+        </div>
+    </div>
+
+    {{-- Category --}}
+    <div class="mb-3 row">
+        <label for="category_id" class="col-sm-2 col-form-label">Category</label>
+        <div class="col-sm-10">
+            <select name="category_id" id="category_id" class="form-select" required>
+                <option disabled selected>-- Select Category --</option>
                 @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
                 @endforeach
             </select>
         </div>
+    </div>
 
-        <div class="form-group mb-3">
-            <label for="year">Year</label>
-            <input type="number" name="year" class="form-control" required>
+    {{-- Year --}}
+    <div class="mb-3 row">
+        <label for="year" class="col-sm-2 col-form-label">Year</label>
+        <div class="col-sm-10">
+            <input type="number" name="year" class="form-control" id="year" required>
         </div>
+    </div>
 
-        <div class="form-group mb-3">
-            <label for="actors">Actors</label>
-            <input type="text" name="actors" class="form-control" required>
+    {{-- Actors --}}
+    <div class="mb-3 row">
+        <label for="actors" class="col-sm-2 col-form-label">Actors</label>
+        <div class="col-sm-10">
+            <input type="text" name="actors" class="form-control" id="actors"  required>
         </div>
+    </div>
 
-        <div class="form-group mb-3">
-            <label for="cover_image">Cover Image</label>
-            <input type="file" name="cover_image" class="form-control" accept="image/*" required>
+
+
+    {{-- Cover Image Upload --}}
+    <div class="mb-3 row">
+        <label for="cover_image" class="col-sm-2 col-form-label">Cover Image</label>
+        <div class="col-sm-10">
+            <input type="file" name="cover_image" class="form-control" id="cover_image" required>
         </div>
+    </div>
 
-        <button type="submit" class="btn btn-primary">Simpan</button>
-    </form>
+    <div class="mb-3 row">
+        <div class="col-sm-10 offset-sm-2">
+            <button type="submit" class="btn btn-success">Submit Movie</button>
+        </div>
+    </div>
+</form>
 @endsection
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,29 +1,28 @@
 @extends('layouts.template')
 
+
 @section('content')
-    <h1>Latest Movie</h1>
-    <div class="row">
-        @foreach ($movies as $movie)
-        <div class="col-lg-6">
+<h1>Latest Movies</h1>
+<div class="row">
+    @foreach ($movies as $movie )
+    <div class="col-lg-6">
         <div class="card mb-3">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="{{ $movie->cover_image }}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ $movie->title }}</h5>
-        <p class="card-text">{{ Str::words($movie->synopsis, 20, '...')}}</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-        <a href="/detail-movie/{{ $movie->id }}/{{ $movie->slug }}" class="btn btn-success">See More</a>
-
-      </div>
-    </div>
-  </div>
-</div>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ asset($movie->cover_image) }}" class="img-fluid rounded-start" alt="{{ $movie->title }}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <p class="card-text">{{Str::words($movie->synopsis,20,end:'...')}}</p>
+                        <a href="{{ route('movie.detail', ['id' => $movie->id, 'slug' => Str::slug($movie->title)]) }}" class="btn btn-success">See More</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        @endforeach
-        {{ $movies->links() }}
     </div>
-@endsection
+    @endforeach
+    {{ $movies->links()}}
+</div>
 
+@endsection
